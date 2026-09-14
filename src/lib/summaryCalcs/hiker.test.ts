93d9eb9044b915b2161ba16087e06ee1b2c942be
Rick Vermeil <rickv85@gmail.com>
2026-09-14 14:35:04 -0600

    Update getAverageFlashTime and tests

--- src/lib/summaryCalcs/hiker.test.ts
+++ src/lib/summaryCalcs/hiker.test.ts
@@ -4,26 +4,27 @@ import { getAverageFlashTime } from './hiker';
 
 test('it should return average 24HR UTC time of day for an array of flashes', () => {
   const result = getAverageFlashTime(flashesSample);
-  expect(result).toBe('08:00');
+  expect(result).toBe('14:00');
 });
 
 test('it should return the time of the flash if only one flash is returned', () => {
   const result = getAverageFlashTime([flashesSample[1]]);
-  expect(result).toBe('04:00');
+  expect(result).toBe('10:00');
 });
 
-test('it should correctly round to the nearest minute', () => {
-  const shouldRoundDown = [
-    { flash_time: '2026-08-01T00:00:00.000000Z' },
-    { flash_time: '2026-08-01T00:02:59.999999Z' },
-  ] as Flash[];
-
-  const shouldRoundUp = [
+test('it should truncate the time, removing seconds from the average time', () => {
+  const case1 = [
     { flash_time: '2026-08-01T00:00:00.000000Z' },
     { flash_time: '2026-08-01T00:03:00.000000Z' },
   ] as Flash[];
-  expect(getAverageFlashTime(shouldRoundDown)).toBe('00:01');
-  expect(getAverageFlashTime(shouldRoundUp)).toBe('00:02');
+
+  const case2 = [
+    { flash_time: '2026-08-01T00:00:00.000000Z' },
+    { flash_time: '2026-08-01T00:03:59.000000Z' },
+  ] as Flash[];
+
+  expect(getAverageFlashTime(case1)).toBe('06:01');
+  expect(getAverageFlashTime(case2)).toBe('06:01');
 });
 
 // Sad paths
@@ -43,5 +44,5 @@ test('it should skip flashes that are not ISO strings', () => {
     {},
     { flash_time: '2026-08-01T01:00:00.000000Z' },
   ] as Flash[];
-  expect(getAverageFlashTime(badData1)).toBe('00:30');
+  expect(getAverageFlashTime(badData1)).toBe('06:30');
 });
--- src/lib/summaryCalcs/hiker.ts
+++ src/lib/summaryCalcs/hiker.ts
@@ -4,9 +4,9 @@ export function getAverageFlashTime(flashes: Record<string, unknown>[]): string 
   if (!flashes || !flashes.length) return null;
   const totals = flashes.reduce(
     (result: { totalMs: number; validCount: number }, flash) => {
-      const flashIsoString = flash.flash_time;
-      if (!flashIsoString || typeof flashIsoString !== 'string') return result;
-
+      if (!flash.flash_time || typeof flash.flash_time !== 'string') return result;
+      // Create MDT offset ISO string with 3 digit precision (from 6 digit UTC)
+      const flashIsoString = flash.flash_time.slice(0, 23) + '-0600';
       const flashMidnightEpoch = new Date(flashIsoString.slice(0, 10)).getTime();
       if (Number.isNaN(flashMidnightEpoch)) return result;
 
@@ -29,10 +29,8 @@ export function getAverageFlashTime(flashes: Record<string, unknown>[]): string 
   if (averageTimeOfDayMs > 0 && averageTimeOfDayMs <= ONE_DAY_IN_MS) {
     // MS are floored by ignoring them below, precision not needed at that level
     const resultDate = new Date(averageTimeOfDayMs);
-    const seconds = resultDate.getUTCSeconds();
-    const roundUpMinutes = seconds < 30 ? 0 : 1;
     const hours = String(resultDate.getUTCHours()).padStart(2, '0');
-    const minutes = String(resultDate.getUTCMinutes() + roundUpMinutes).padStart(2, '0');
+    const minutes = String(resultDate.getUTCMinutes()).padStart(2, '0');
     return `${hours}:${minutes}`;
   }
   return null;
